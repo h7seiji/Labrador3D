58ada6b5f75befac259f574d4ec8e1ea7002b293
ryuuji06 <39686533+ryuuji06@users.noreply.github.com>
2018-11-13 18:13:05 -0200

    Create build_sjpeg.sh

---
+++ build_sjpeg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd sjpeg
+mkdir build
+cd build
+cmake . ..
+make -j4
+
+# make a copy inside appBoard folder
+cp sjpeg ../../appBoard/sjpeg
+
+# using
+# ./sjpeg image.jpg -o output.jpg -r 50
