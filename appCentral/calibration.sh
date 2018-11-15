b8fb51d2fce993bee60f81a23b7ac0afa9c2b8ea
ryuuji06 <39686533+ryuuji06@users.noreply.github.com>
2018-11-15 14:30:16 -0200

    Update calibration.sh

--- appCentral/calibration.sh
+++ appCentral/calibration.sh
@@ -3,7 +3,7 @@
 # ================================================
 # ============= P A R A M E T R O S ==============
 # ================================================
-N=5	# numero de placas
+N=$1	# numero de placas
 R=2	# resolucao da deteccao (0,1,2)
 F=612	# distancia focal da camera
 TESTE=1	# usa imagens de teste ou nao (0,1)
@@ -14,17 +14,17 @@ echo "Number of boards: $N"
 
 
 
-START=$(date +%s)
+START=$(date +%s%N)
 ./bin/dataInit -f $F -n $N -w 640 -h 480
 ./bin/takePictures $N $R $TESTE
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo -e "Distributed Feature Detection Time: $DIFF seconds\n"
+END=$(date +%s%N)
+DIFF=$(( ($END - $START)/1000000 ))
+echo -e "Distributed Feature Detection Time: $DIFF ms\n"
 
-START=$(date +%s)
+START=$(date +%s%N)
 ./bin/sequentialSFM -p $R
-END=$(date +%s)
-DIFF2=$(( $DIFF + $END - $START ))
-echo -e "\nCalibration Total Time: $DIFF2 seconds\n"
+END=$(date +%s%N)
+DIFF2=$(( $DIFF + (($END - $START)/1000000) ))
+echo -e "\nCalibration Total Time: $DIFF2 ms\n"
 
 
