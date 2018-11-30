85a6759b21cd9a6cd0e8e875f2c1b7c0583ed1ba
h7seiji <helder.costa@usp.br>
2018-11-30 15:23:46 -0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,9 +6,7 @@ sudo echo "echo $PASS | sudo bash labrador3d.sh" >> '/home/citi/startup.sh'
 #configure startup script
 #sudo nano .config/lxsession/LXDE/autostart
 sudo echo "@sh /home/citi/startup.sh" >> '/home/citi/.config/lxsession/LXDE/autostart'
-#configure network settings (run with sudo)
-#add last number of ip with
-#sudo nano /etc/network/interfaces
+#configure network settings
 sudo echo "# Interfaces(5) file used by ifup(8) and ifdown(8)" > '/etc/network/interfaces'
 sudo echo "# Include files from /etc/network/interfaces.d" >> '/etc/network/interfaces'
 sudo echo "source-directory /etc/network/interfaces.d" >> '/etc/network/interfaces'
@@ -20,7 +18,9 @@ sudo echo "netmask 255.255.255.0" >> '/etc/network/interfaces'
 sudo echo "" >> '/etc/network/interfaces'
 sudo echo "#auto eth0" >> '/etc/network/interfaces'
 sudo echo "#iface eth0 inet dhcp" >> '/etc/network/interfaces'
+#manually assign different port numbers for each board
 sudo nano /home/citi/labrador3d/appBoard/labrador3d.sh
+#manually assign a unique valid IP address for the board (just add the last number)
 sudo nano /etc/network/interfaces
 sudo ifconfig eth0 up
 sudo /etc/init.d/networking restart
