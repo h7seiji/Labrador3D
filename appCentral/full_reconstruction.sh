81d9b0e95b58308a34f30e00633358117bdcb21f
ryuuji06 <39686533+ryuuji06@users.noreply.github.com>
2018-12-01 07:28:26 -0200

    Update full_reconstruction.sh

--- appCentral/full_reconstruction.sh
+++ appCentral/full_reconstruction.sh
@@ -1,19 +1,26 @@
 #!/bin/bash 
 
-# ================================================
-# ============= P A R A M E T R O S ==============
-# ================================================
+# ==================================================
+# ============== P A R A M E T R O S ===============
+# ==================================================
 N=$1	# numero de placas
 R=2	# resolucao da deteccao (0,1,2)
 F=612	# distancia focal da camera
 TESTE=1	# usa imagens de teste ou nao (0,1)
-# ================================================
+
+# ==================================================
+# ================  O U T R O S  ===================
+# ==================================================
+NET=enp3s0  # nome da conexao (ver com ifconfig)
+PASS=xxx    # senha do usuario para usar 'sudo'
+
+# ==================================================
 
 echo -e "===================================================\n=============== FULL RECONSTRUCTION ===============\n===================================================\n"
 echo "Number of boards: $N"
 
-
-
+# ========================================================
+# INICIALIZACAO, COMUNICACAO COM AS PLACAS
 START=$(date +%s%N)
 ./bin/dataInit -f $F -n $N -w 640 -h 480
 ./bin/takePictures $N $R $TESTE
@@ -21,6 +28,27 @@ END=$(date +%s%N)
 DIFF1=$((($END - $START)/1000000))
 echo -e "Distributed Feature Detection Time: $DIFF1 ms\n"
 
+# ========================================================
+# Desativa conexao ethernet
+echo $PASS | sudo -S ip link set down $NET
+
+# Cria arquivo TXT para registrar tempos de processamento
+NUM=1
+ONE=1
+WRITE="1"
+while [ $WRITE != "0" ]; do
+  if [ ! -e "result$NUM.txt" ]
+  then
+    FILE="result$NUM.txt"
+    touch $FILE
+    WRITE="0"
+  else
+    NUM=$(($NUM + $ONE))
+  fi
+done
+
+# ========================================================
+# STRUCTURE FROM MOTION
 START=$(date +%s%N)
 ./bin/sequentialSFM -p $R
 ./bin/export2MVE
@@ -28,6 +56,7 @@ START=$(date +%s%N)
 END=$(date +%s%N)
 DIFF2=$((($END - $START)/1000000))
 
+# ========================================================
 # MVE dmrecon needs at least 5 images
 # MVS-Texturing may gives some error for few images
 echo -e "\n\nDEPTHMAP RECONSTRUCTION\n"
@@ -64,4 +93,14 @@ echo -e "Texturing Time: $DIFF7 ms\n"
 TOTAL=$(( $DIFF1 + $DIFF2 + $DIFF3 + $DIFF4 + $DIFF5 + $DIFF6 + $DIFF7 ))
 echo -e "\nFull Reconstruction Total Time: $TOTAL ms\n"
 
+echo -e "\nFULL RECONSTRUCTION TEST: NETWORK\n" >> $FILE
+echo -e "Feature Detection: $DIFF1 ms" >> $FILE
+echo -e "Depthmap Recon: $DIFF3 ms" >> $FILE
+echo -e "Exporting to pointcloud: $DIFF4 ms" >> $FILE
+echo -e "Surface Recon: $DIFF5 ms" >> $FILE
+echo -e "Surface Cleaning: $DIFF6 ms" >> $FILE
+echo -e "Texturing: $DIFF7 ms" >> $FILE
+echo -e "Total Time: $TOTAL ms" >> $FILE
 
+# Reconecta conexao ethernet
+sudo ip link set up $NET
